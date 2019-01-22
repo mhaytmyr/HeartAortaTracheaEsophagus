f83f415d2ce52884e15fcb84859beb530eeedfb8
upsilon <mhaytmuradov@gmail.com>
2019-01-22 14:57:10 -0600

    moved some methods to classes

--- helper_to_processing.py
+++ helper_to_processing.py
@@ -15,14 +15,110 @@ class Normalizer:
         self.normalization = normalize;
 
     def normalize(self,inputImg):
+        '''
+        Method to normalize image using predtermined normalization factor
+        input: ndarray
+        output: ndarray (normalized)
+        '''
         nonZero = np.ma.masked_equal(inputImg,0)
         normalized = ((nonZero-self.normalization["means"])/self.normalization["vars"]).data
         return normalized
 
     def denormalize(self,inputImg):
+        '''
+        Method to apply inverse normalization to image
+        input: ndarray
+        output: ndarray
+        '''
         nonZero = np.ma.masked_equal(inputImg,0)
         image = (nonZero*self.normalization["vars"]+self.normalization["means"]).data
         return image
+
+    def standardize_nii(self,niiObject,minHU=-1000, maxHU=3000):
+        '''
+        Pre-process images by clipping outliers and shifting -1000 to 0
+        Input: nibabel object (512,512,num_slices)
+        Output: normalized ndarray  
+        '''
+        #1. convert nii object to ndarray
+        img = niiObject.get_data()
+
+        #2. standardiza dimension from [H,W,N] to [N,H,W] 
+        img = np.transpose(img,[2,0,1])
+
+        #print("CT before clipping ...",img.max(), img.min())
+        #2. nii already converted to HU's
+        imgClip = (img.clip(minHU,maxHU)+1000).astype('uint16')
+        #print("CT after clipping ...",sliceHU.max(), sliceHU.min())
+
+        #3. images are rotated vertically correct them here
+        imgRot = np.rot90(imgClip,k=3,axes=(1,2)); 
+
+        return imgRot
+
+    def de_standardize_nii(self,imgInput):
+        '''
+        Method to convert nii images back to original format
+        inputs: ndarray
+        output: ndarray (TODO: conevrt to nii object for submission)
+        '''
+        #1. rotate back image as above
+        imgRot = np.rot90(imgInput,k=-3,axes=(1,2))    
+        #2. convert [N,H,W] -> [H,W,N]
+        imgRot = np.transpose(imgRot,[1,2,0])
+
+        return imgRot
+    
+    def standardize_nii_label(self,label):
+        '''
+        Pre-process labels by rotating horizontally    
+        Input: niibabel object (512,512,num_slices)
+        Output: standardized ndimage 
+        '''
+        #1. convert nii to ndarray
+        label = label.get_data()
+
+        #3. standardiza dimension from [H,W,N] to [N,H,W] 
+        label = np.transpose(label,[2,0,1])
+
+        #2. roate label counter-clockwise
+        labelRot = np.rot90(label,k=3,axes=(1,2)) 
+
+        return labelRot
+
+    def standardize_dicom(self,imgSlice,minHU=-1000, maxHU=3000):
+        '''
+        Converts pixel values to CT values, then shifts everything by 1000 to make air zero
+        Input: pydicom object
+        Outpur: ndarray    
+        '''
+        #1. convert pixel data to HU
+        slope = imgSlice.RescaleSlope
+        intercept = imgSlice.RescaleIntercept
+        sliceHU = imgSlice.pixel_array*slope+intercept
+
+        #print("Before clipping ",sliceHU.max(), sliceHU.min(), sliceHU.dtype);
+        #2 clip HU between [-1000, 3000]
+        sliceHU = (sliceHU.clip(minHU,maxHU)+1000).astype('uint16')
+    
+        return sliceHU.astype('float32')
+
+    def standardize_dicom_label(self,labelMask,removeBody=False, organToSegment=False):
+        '''
+        Method to standardize labels; such as removing body label or binarizing labels
+        '''
+        if organToSegment:
+            labelMask[labelMask!=organToSegment] = 0;
+            labelMask[labelMask==organToSegment] = 1;
+
+        if removeBody:
+            #1. find non zero mask
+            nonAir = ~(labelMask==0);    
+            #2. subtract one from index, 
+            labelMask[nonAir] = labelMask[nonAir]-1; 
+    
+        return labelMask.astype("float32");
+
 
 class Cropper:
     def __init__(self):
@@ -155,9 +251,9 @@ class ImageProcessor(Normalizer,Cropper):
         #first convert image to numpy array
         if type(inputFile)==nib.nifti1.Nifti1Image:#input file is nii
             print("NII chosen")
-            imgStandard = standardize_nii(inputFile)
+            imgStandard = self.standardize_nii(inputFile)
         elif type(inputFile)==pydicom.dataset.FileDataset:#input file is dcm
-            imgStandard = standardize_dicom(inputFile)
+            imgStandard = self.standardize_dicom(inputFile)
         elif type(inputFile)==np.ndarray:#input must be already standardized
             imgStandard = inputFile
         else:
@@ -168,9 +264,9 @@ class ImageProcessor(Normalizer,Cropper):
         #first convert image to numpy array
         if type(inputFile)==nib.nifti1.Nifti1Image:#input file is nii
             print("Nii Label chosen")
-            labelStandard = standardize_nii_label(inputFile)
+            labelStandard = self.standardize_nii_label(inputFile)
         elif type(inputFile)==pydicom.dataset.FileDataset:#input file is dcm
-            labelStandard = standardize_dicom_label(inputFile)
+            labelStandard = self.standardize_dicom_label(inputFile)
         elif type(inputFile)==np.ndarray:#input must be already standardized
             labelStandard = inputFile
         else:
@@ -265,75 +361,4 @@ def crop_image_roi(img,rowMin=ROW,rowMax=ROW+W,colMin=COL,colMax=COL+H):
         
 
 
-def standardize_nii(img,minHU=-1000, maxHU=3000):
-    '''
-    Pre-process images by clipping outliers and shifting -1000 to 0
-    Input: nibabel object (512,512,num_slices)
-    Output: normalized ndarray  
-    '''
-    #1. convert nii object to ndarray
-    img = img.get_data()
-
-    #2. standardiza dimension from [H,W,N] to [N,H,W] 
-    img = np.transpose(img,[2,0,1])
-
-    #print("CT before clipping ...",img.max(), img.min())
-    #2. nii already converted to HU's
-    sliceHU = (img.clip(minHU,maxHU)+1000).astype('uint16')
-    #print("CT after clipping ...",sliceHU.max(), sliceHU.min())
-
-    #3. images are rotated vertically correct them here
-    imgRot = np.rot90(sliceHU,k=3,axes=(1,2)); 
-
-    return imgRot
-
-def standardize_nii_label(label):
-    '''
-    Pre-process labels by rotating horizontally    
-    Input: niibabel object (512,512,num_slices)
-    Output: standardized ndimage 
-    '''
-    #1. convert nii to ndarray
-    label = label.get_data()
-
-    #3. standardiza dimension from [H,W,N] to [N,H,W] 
-    label = np.transpose(label,[2,0,1])
-
-    #2. roate label counter-clockwise
-    labelRot = np.rot90(label,k=3,axes=(1,2)) 
-
-    return labelRot
-
-def standardize_dicom(imgSlice,minHU=-1000, maxHU=3000):
-    '''
-    Converts pixel values to CT values, then shifts everything by 1000 to make air zero
-    Input: pydicom object
-    Outpur: ndarray    
-    '''
-    #1. convert pixel data to HU
-    slope = imgSlice.RescaleSlope
-    intercept = imgSlice.RescaleIntercept
-    sliceHU = imgSlice.pixel_array*slope+intercept
-
-    #print("Before clipping ",sliceHU.max(), sliceHU.min(), sliceHU.dtype);
-    #2 clip HU between [-1000, 3000]
-    sliceHU = (sliceHU.clip(minHU,maxHU)+1000).astype('uint16')
-    
-    return sliceHU.astype('float32')
-
-def standardize_dicom_label(labelMask,removeBody=False, organToSegment=False):
-    '''
-    Method to standardize labels; such as removing body label or binarizing labels
-    '''
-    if organToSegment:
-        labelMask[labelMask!=organToSegment] = 0;
-        labelMask[labelMask==organToSegment] = 1;
-
-    if removeBody:
-        #1. find non zero mask
-        nonAir = ~(labelMask==0);    
-        #2. subtract one from index, 
-        labelMask[nonAir] = labelMask[nonAir]-1; 
-    
-    return labelMask.astype("float32");
 
