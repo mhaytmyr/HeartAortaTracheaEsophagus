7bbe2f956594a9b3826b01550b752dbb4c06b53d
upsilon <mhaytmuradov@gmail.com>
2019-01-24 07:44:01 -0600

    added plotter methods to debug prediction

--- helper_to_validate.py
+++ helper_to_validate.py
@@ -36,7 +36,7 @@ class Plotter:
         k = cv2.waitKey(0);
         return k
 
-    def tensor_to_image_wrapper(self,imgInput=None):
+    def tensor_to_image_wrapper(self,imgInput=None,idx=None):
         #Method to convert 4D array image to 2D image
         #first remove extra dimentions
         imgInput = imgInput.squeeze()
@@ -44,15 +44,21 @@ class Plotter:
         if len(imgInput.shape)==2:
             imgNorm = (imgInput-imgInput.min())/imgInput.max()
         elif len(imgInput.shape)==3:
-            idx = np.random.randint(0,imgInput.shape[0]-1)
+            if idx is None:
+                idx = np.random.randint(0,imgInput.shape[0]-1)
             imgNorm = (imgInput[idx]-imgInput[idx].min())/imgInput[idx].max()
         else:
             sys.exit(type(self).__name__+".tensor_to_image_wrapper() can't convert input to image "+str(imgInput.shape)) 
         return imgNorm
 
-    def tensor_to_label_wrapper(self,labelInput=None):
-        #TODO: method to convert 4D label array to 2D image'
-        pass
+    def tensor_to_label_wrapper(self,labelInput=None,idx=None):
+        #Method to convert 4D label array to 2D image'
+        labelInput = labelInput.squeeze()
+        #apply argmax to last index, since prediction is one-hot encoded
+        print(np.unique(labelInput))
+        if len(labelInput.shape)==4:
+            labelInput = labelInput.argmax(axis=-1) 
+        return self.tensor_to_image_wrapper(labelInput,idx)
 
     def plot_slice(self,imgInput):
         #method to plot image slice only
@@ -64,11 +70,19 @@ class Plotter:
 
 
     def plot_slice_label(self,imgInput,labelInput):
-        #TODO: method to plot image and label masks
-        pass
+        #Method to plot image and label masks
+        #input is 4D array, so we need to choose one
+        idx =  np.random.randint(0,imgInput.shape[0]-1)
+        img = self.tensor_to_image_wrapper(imgInput,idx=idx)
+        label = self.tensor_to_label_wrapper(labelInput,idx=idx)
+
+        imgNorm = np.hstack([img,label])
+        cv2.imshow("Slice ",imgNorm)
+        k = cv2.waitKey(0)
+        return k
 
     def plot_label_prediction(self,imgInput,labelInput,predInput):
-        #TODO: method to compare ground truth and prediction contourd
+        #Method to compare ground truth and prediction contours
         pass
 
 
@@ -121,23 +135,16 @@ class SubmitPrediction:
             slices = nib.load(item);   
                    
             #predict image patch
-            k = self.predict_image_slices(slices,batchSize,processor,plotter)
+            #k = self.predict_image_slices(slices,batchSize,processor,plotter)
+            k = self.processor_tester(slices,batchSize,processor,plotter)
             if (k==27):
                 break
         pass            
 
-
-    #method to extract slices from image batches and predict
-    def predict_image_slices(self,slices,batchSize=16,processor=None,plotter=None):
-        
-        #initialize pre-processor class
-        if processor is None:
-            sys.exit(type(self).__name__+".predict_image_slices() needs ImageProcessor class")
-
+    def processor_tester(self,slices,batchSize=6,processor=None,plotter=None):
         #apply preprocessing
         imgStandard = processor.standardize_img(slices)
         imgDeStandard = processor.de_standardize_nii(imgStandard)
-        print(imgDeStandard.shape)
         imgCrop = processor.crop(imgStandard)
         imgZoom = processor.zoom(imgCrop)
         imgNorm = processor.normalize(imgZoom)
@@ -146,26 +153,48 @@ class SubmitPrediction:
         imgDeNorm = processor.denormalize(imgNorm)
         imgUnZoom = processor.unzoom(imgDeNorm)
         imgDeCrop = processor.uncrop(imgUnZoom)
-
         
         n = imgNorm.shape[0]
 
         for idx in range(0,n,batchSize):
-            #get batch of image
-            #imgBatch = imgInput[idx:idx+batchSize] 
-            imgBatch = imgDeNorm[idx:idx+batchSize]
+            #get batch of image 
+            imgBatch = imgNorm[idx:idx+batchSize]
             
-            imgZoomUnZoom = [imgCrop[idx],imgUnZoom[idx]]
+            #imgZoomUnZoom = [imgCrop[idx],imgUnZoom[idx]]
             #print(abs(imgCrop[idx]-imgUnZoom[idx]).max())
-            imgCropUnCrop = [imgStandard[idx],imgDeCrop[idx],imgStandard[idx]-imgDeCrop[idx]]
-            imgStandUnStand = [imgStandard[idx],imgDeStandard[...,idx]]
-
+            #imgCropUnCrop = [imgStandard[idx],imgDeCrop[idx],imgStandard[idx]-imgDeCrop[idx]]
+            #imgStandUnStand = [imgStandard[idx],imgDeStandard[...,idx]]
+            labelPred = self.model.predict(processor.img_to_tensor(imgBatch))
+            labelPredUnZoom = processor.unzoom(labelPred.argmax(axis=-1))
+            labelPredDeCrop = processor.uncrop(labelPredUnZoom)
+            
             print(idx,idx+batchSize,imgBatch.shape)
-            #k = plotter.plot(imgBatch)
+            #k = plotter.plot_slice_label(imgDeNorm[idx:idx+batchSize],labelPred)
+            k = plotter.plot_slice_label(imgDeCrop[idx:idx+batchSize],labelPredDeCrop)
             #k = plotter.plot_slices(imgCropUnCrop)
             #k = plotter.plot_slices(imgZoomUnZoom)
-            k = plotter.plot_slices(imgStandUnStand)
+            #k = plotter.plot_slices(imgPred)
             if (k==27) or (k=='n'):
                 return k
         return k
+
+    #method to extract slices from image batches and predict
+    def predict_image_slices(self,slices,batchSize=16,processor=None,plotter=None):
+        
+        #initialize pre-processor class
+        if processor is None:
+            sys.exit(type(self).__name__+".predict_image_slices() needs ImageProcessor class")
+
+        #first call standardize method which converts nii or dcm to numpy
+        imgStandard = processor.standardize_img(slices)
+
+        n = imgStandard.shape[0]
+        for idx in range(0,n,batchSize):
+            #process image batch
+            imgBatch = processor.pre_process(imgStandard[idx:idx+batchSize])
+            print("Predicting...",idx,idx+batchSize,imgBatch.shape)
+            #predict image
+            imgPred = self.model.predict(processor.img_to_tensor(imgBatch))
+            
+        
                 
